be4b8346e48cfbaed2e13cbb76f0a292c8bfef4f
Muhammad Abbas <muhammdabbas.cs@gmail.com>
2026-06-28 21:56:12 +0500

    dbt init, bronze, silver layers and snapshots done

---
+++ airstats/models/silver/silver_runways.sql
@@ -0,0 +1,12 @@
+SELECT 
+    runway_id,
+    airport_ident,
+    runway_length_ft,
+    runway_width_ft,
+    CASE 
+    WHEN runway_surface IS NULL OR runway_surface = '' THEN '__UNKNOWN__' ELSE runway_surface
+    END AS runway_surface,
+    runway_lighted,
+    runway_closed
+FROM 
+    {{ ref('src_runways') }}
